306ada623ba4a8d583755cc9c3719dc6475bb912
vaidehee patel <pvaidehee@gmail.com>
2021-06-04 13:30:14 -0400

    don't ever forget to commit reg

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,2 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Howard', 'Harrison'),('Jim', 'Harrison'),('Jones', 'McGee'),('Steven', 'Redgrave'),('Will', 'Bower');
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('Fanta', 1.99),('Sprite', 1.99),('Coca Cola', 5.03),('Orange Juice', 1.99),('Apple Juice', 2.01);
